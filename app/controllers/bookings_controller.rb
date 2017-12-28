07acf183a9c850a45c5e90b70f0f5254082d1f31
rasul <rasulbiguduru@gmail.com>
2017-12-28 11:04:50 +0530

    ui comit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,27 +7,30 @@ class BookingsController < ApplicationController
     @booking=Booking.new
     @slot=Slot.find(params[:name])
      #@available_slots = Interval.where(:slot_id => @slot.id)
-      @available_slots = Interval.where(:slot_id => @slot.id).where.not(:is_available => false).collect{|i| [i.timeslot , i.id]}
+      #@available_slots = Interval.where(:slot_id => @slot.id).where.not(:is_available => false).collect{|i| [i.timeslot , i.id]}
+      @available_slots = Interval.where(:slot_id => @slot.id).collect{|i| [i.timeslot , i.id, i.is_available]}
   end
   def createBooking
     @booking=Booking.new(booking_params)
     @booking.hospital_id = Slot.where(:id => @booking.slot_id).first.hospital.id
     @booking.doctor_id = Slot.where(:id => @booking.slot_id).first.doctor.id
     @booking.user_id = current_user.id
-    byebug
     if Interval.where(:id => @booking.slot_time.to_i).first.is_available == true
     if @booking.save
       @updateColumn = Booking.update_is_available_to_true @booking
       @updateSlot = @updateColumn[0]
       @updateInterval = @updateColumn[1]
       @updateInterval.save
-      @updateSlot.save
-      redirect_to home_mybookings_path
+      if @updateSlot.save
+        flash[:success] = "Slot Created Successfully!"
+        redirect_to home_mybookings_path
+      else
+        flash[:success] = "Error in Creating Slot!please try again :("
+      end
     else
       redirect_to root_path
     end
   else
-    byebug
     redirect_to bookings_book_path(:name => @booking.slot_id)
     flash[:error] = "slot is not available try with another slot"
   end
